df81a58781918c0c250f83fabd914251ad525859
Mario Lyon <mario@digileo.com>
2020-03-05 12:45:22 +0100

    Unit test the behaviour of loading new values into cache

--- src/test/scala/com/digileo/CacheBehaviorSpec.scala
+++ src/test/scala/com/digileo/CacheBehaviorSpec.scala
@@ -17,13 +17,22 @@ class CacheBehaviorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike w
     }
   })
 
-  "CacheBehavior" should {
-    "tell the Loader to load values if the value is not in the cache" in {
+  "Cache" should {
+    "call Loader enough times to be able to retrieve a queried item" in {
       var loadCalls:Int = 0
       val mockLoader = testKit.spawn(Behaviors.receiveMessage[Loader.LoadRequest] {
         case Loader.LoadRequest(replyTo: ActorRef[Cache.LoadResponse]) => {
+          val result = if(loadCalls == 0) {
+            List('A', 'B')
+          } else if(loadCalls == 1) {
+            List('C', 'D', 'E')
+          } else {
+            List()
+          }
+
           loadCalls += 1
-          replyTo ! Cache.LoadResponse(List.empty)
+          replyTo ! Cache.LoadResponse(result)
+
           Behaviors.same
         }
       })
@@ -36,9 +45,22 @@ class CacheBehaviorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike w
       val cacheActor = testKit.spawn(cacheBehavior)
 
       val probe = testKit.createTestProbe[Answer]()
+      cacheActor ! Cache.Question(4, probe.ref)
+      probe.expectMessage(Some('E'))
+      loadCalls should equal(2)
+
+      // Getting an item already in the cache should not trigger a call to Loader.
       cacheActor ! Cache.Question(0, probe.ref)
+      probe.expectMessage(Some('A'))
+      loadCalls should equal(2)
+
+      // If an unknown item is requested and the Load call returns no data the answer
+      // should be None, and only 1 additional call to Loader should be made;
+      // In other words we should not get an infinite loop.
+      cacheActor ! Cache.Question(5, probe.ref)
       probe.expectMessage(None)
-      loadCalls should equal(1)
+
+      loadCalls should equal(3)
     }
   }
 }
