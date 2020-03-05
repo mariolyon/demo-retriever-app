b911a01e0de19877c6f4a174cb46cc81f744ac6a
Mario Lyon <mario@digileo.com>
2020-03-05 11:09:24 +0100

    Add proper unit tests for AppRoutes

--- src/main/scala/com/digileo/AppRoutes.scala
+++ src/main/scala/com/digileo/AppRoutes.scala
@@ -1,30 +1,22 @@
 package com.digileo
 
-import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.actor.typed.scaladsl.AskPattern._
+import akka.actor.typed.{ActorRef, ActorSystem}
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{RejectionHandler, Route}
 import akka.util.Timeout
 
 import scala.concurrent.Future
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import JsonFormats._
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.model.headers.HttpEncoding
 
 class AppRoutes(cache: ActorRef[Cache.Command])(implicit val system: ActorSystem[_]) {
   private implicit val timeout = Timeout.create(system.settings.config.getDuration("app.routes.ask-timeout"))
 
   def getValue(index: Int): Future[Answer] = cache.ask(Cache.Question(index, _))
 
+  implicit def rejectionHandler = RejectionHandler.default
+
   val appRoutes: Route = concat(
-    path("hi") {
-      get {
-        complete {
-          "hello"
-        }
-      }
-    },
     path(IntNumber) { index: Int =>
       get {
           val result = getValue(index)
--- src/test/scala/com/digileo/AppRoutesSpec.scala
+++ src/test/scala/com/digileo/AppRoutesSpec.scala
@@ -1,6 +1,8 @@
 package com.digileo
 
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.typed.ActorRef
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
@@ -14,19 +16,59 @@ class AppRoutesSpec extends WordSpec with Matchers with ScalaFutures with Scalat
   override def createActorSystem(): akka.actor.ActorSystem =
     testKit.system.toClassic
 
-  val cache = testKit.spawn(Cache())
-  lazy val routes = new AppRoutes(cache).appRoutes
-
   "AppRoutes" should {
-    "return value" in {
+    "reject request if route is called with a non-numeric string" in {
+      val mockCache = testKit.spawn(Behaviors.receiveMessage[Cache.Command] {
+        case Cache.Question(_: Int, replyTo: ActorRef[Answer]) => {
+          replyTo ! Some('B')
+          Behaviors.same
+        }
+      })
+
+      lazy val routes = new AppRoutes(mockCache).appRoutes
+
+      val request = HttpRequest(uri = "/xyz")
+
+      val result = request ~> routes
+      result.handled should equal(false)
+    }
+
+    "respond with OK and value if Cache answers with a value " in {
+      val mockCache = testKit.spawn(Behaviors.receiveMessage[Cache.Command] {
+        case Cache.Question(_: Int, replyTo: ActorRef[Answer]) => {
+          replyTo ! Some('B')
+          Behaviors.same
+        }
+      })
+
+      lazy val routes = new AppRoutes(mockCache).appRoutes
+
       val request = HttpRequest(uri = "/0")
 
       request ~> routes ~> check {
         status should ===(StatusCodes.OK)
         contentType should ===(ContentTypes.`text/plain(UTF-8)`)
-        entityAs[String] should ===("A")
+        entityAs[String] should ===("B")
       }
     }
 
+    "respond with InternalServerError and empty string if Cache answers with None " in {
+      val mockCache = testKit.spawn(Behaviors.receiveMessage[Cache.Command] {
+        case Cache.Question(_: Int, replyTo: ActorRef[Answer]) => {
+          replyTo ! None
+          Behaviors.same
+        }
+      })
+
+      lazy val routes = new AppRoutes(mockCache).appRoutes
+
+      val request = HttpRequest(uri = "/0")
+
+      request ~> routes ~> check {
+        status should ===(StatusCodes.InternalServerError)
+        contentType should ===(ContentTypes.`text/plain(UTF-8)`)
+        entityAs[String] should ===("")
+      }
+    }
   }
 }
