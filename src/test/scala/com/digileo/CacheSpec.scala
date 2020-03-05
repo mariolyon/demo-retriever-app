42a418944f6a2229571353047a0792da1efe9426
Mario Lyon <mario@digileo.com>
2020-03-05 13:15:56 +0100

    Add Unit tests for the Cache

---
+++ src/test/scala/com/digileo/CacheSpec.scala
@@ -0,0 +1,32 @@
+package com.digileo
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class CacheSpec extends AnyWordSpecLike with Matchers {
+  "Cache" should {
+    "start with an empty cache" in {
+      val cache = new Cache()
+      cache.get(0) should equal(None)
+    }
+
+    "return None if an item is not in the cache" in {
+      val cache = new Cache()
+      cache.add(List('A', 'B'))
+      cache.get(2) should equal(None)
+    }
+
+    "return the wrapped value if an item is in the cache" in {
+      val cache = new Cache()
+      cache.add(List('A', 'B'))
+      cache.get(1) should equal(Some('B'))
+    }
+
+    "mutate it's internal state with successive calls to its add method" in {
+      val cache = new Cache()
+      cache.add(List('A', 'B'))
+      cache.add(List('C', 'D'))
+      cache.get(3) should equal(Some('D'))
+    }
+  }
+}
